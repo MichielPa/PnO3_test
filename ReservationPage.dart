e681185f9658adb67a751e13af2aa09da5dbda68
MichielPa <115628873+MichielPa@users.noreply.github.com>
2022-12-19 11:55:15 +0100

    Add files via upload

--- ReservationPage.dart
+++ ReservationPage.dart
@@ -6,6 +6,7 @@ import 'ReservationButton.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 void main() {  // a simple functions which allows us to open the app
   runApp(const ProviderScope(child: MyApp()));
@@ -23,6 +24,10 @@ class _ReservationPage extends State<ReservationPage> {
   final ScrollController _scrollController = ScrollController();
   int maxPlaces = 3;
 
+
+  Future<void> _handleRefresh() async{
+    return await Future.delayed(const Duration(seconds: 2));
+  }
   @override
   Widget build(BuildContext context) {
     _scrollController.addListener(() {
@@ -43,58 +48,67 @@ class _ReservationPage extends State<ReservationPage> {
           int filledSpots = availability.timestamp;
           return filledSpots;
         }
-      return CustomScrollView(
-          controller: _scrollController,
-          // This widgets makes a list of grids you can scroll through
-          slivers: <Widget>[
-            // list of widgets
-          SliverGrid(
-              // we first define one grid
-              delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                  if (index == timeslots) {
-                    return const CupertinoActivityIndicator();
-                  }
-                  var color = Colors.blue;
-                  var hour = DateFormat('HH:00')
-                      .format(DateTime.now().add(Duration(hours: index)));
-                  // always start with the current time and each
-                  //grid/timeslot is an hour later
-                  // parse changes a string in to an integer
-                  // HH gives the current hour (without minutes or seconds)
-                  var date = DateFormat('dd-MM-yyyy')
-                      .format(DateTime.now().add(Duration(hours: index)));
-                  //Datetime.now() gives us the current time (date, hours, minutes, seconds)
-                  //DateFormat allows us to chose in what way we show the date
-                  return FutureBuilder<int>(
-                      future: freePlaces(index),
-                      // saves it in snapshot
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          int filledSpots = snapshot.data!;
-                          // filledSpots is now equal to the amount of reserved parking
-                          // places during the timestamp
-                          return _buildData(
-                              color, context, index, date, hour, filledSpots);
-                        } else if (snapshot.hasError) {
-                          return Text(snapshot.error.toString());
-                          // there was an error from the back end
-                        } else {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                          // a loading symbol while we wait on the server
-                        }
-                      });
-                },
-                childCount: timeslots + 1,
-                // how many grids/buttons there are
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1, childAspectRatio: 2.7
-                  // how many buttons you can see on the screen at once
-                  )
-          )
-        ],
+      return LiquidPullToRefresh(
+        // this lets us reload the screen by scrolling down from the top
+        onRefresh: _handleRefresh,
+        color: Colors.blueAccent,
+        height: 200,
+        backgroundColor: Colors.white70,
+        animSpeedFactor: 2,
+        showChildOpacityTransition: false,
+        child: CustomScrollView(
+            controller: _scrollController,
+            // This widgets makes a list of grids you can scroll through
+            slivers: <Widget>[
+              // list of widgets
+            SliverGrid(
+                // we first define one grid
+                delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                    if (index == timeslots) {
+                      return const CupertinoActivityIndicator();
+                    }
+                    var color = Colors.blue;
+                    var hour = DateFormat('HH:00')
+                        .format(DateTime.now().add(Duration(hours: index)));
+                    // always start with the current time and each
+                    //grid/timeslot is an hour later
+                    // parse changes a string in to an integer
+                    // HH gives the current hour (without minutes or seconds)
+                    var date = DateFormat('dd-MM-yyyy')
+                        .format(DateTime.now().add(Duration(hours: index)));
+                    //Datetime.now() gives us the current time (date, hours, minutes, seconds)
+                    //DateFormat allows us to chose in what way we show the date
+                    return FutureBuilder<int>(
+                        future: freePlaces(index),
+                        // saves it in snapshot
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            int filledSpots = snapshot.data!;
+                            // filledSpots is now equal to the amount of reserved parking
+                            // places during the timestamp
+                            return _buildData(
+                                color, context, index, date, hour, filledSpots);
+                          } else if (snapshot.hasError) {
+                            return Text(snapshot.error.toString());
+                            // there was an error from the back end
+                          } else {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                            // a loading symbol while we wait on the server
+                          }
+                        });
+                  },
+                  childCount: timeslots + 1,
+                  // how many grids/buttons there are
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1, childAspectRatio: 2.7
+                    // how many buttons you can see on the screen at once
+                    )
+            )
+          ],
+        ),
       );
     });
   }
